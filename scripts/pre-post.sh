d1a83224281d9c84fa1d5270e0469b89f7db5289
lcc2207 <lynn@fidmail.com>
2018-10-22 12:48:22 -0500

    Merge branch 'master' of github.com:lcc2207/scalr-command-webhook

--- scripts/pre-post.sh
+++ scripts/pre-post.sh
@@ -13,19 +13,16 @@ if [ $event == "BeforeInstanceLaunch" ];
  then
   echo "Add host to XXX"
   echo $scalr_server_id >> /opt/hostinfo/ids_hosts
-#  curl --data "host_config_key=16522fa0ee61399df56889b2b3c247f3" http://35.202.229.8:80/api/v2/job_templates/9/callback/
 fi
 
 if [ $event == "HostUp" ];
  then
   echo "adding $hostname to xxx"
   echo $hostname >>  /opt/hostinfo/infra_hosts
-#  curl --data "host_config_key=16522fa0ee61399df56889b2b3c247f3" http://35.202.229.8:80/api/v2/job_templates/8/callback/
 fi
 
 if [ $event == "HostDown" ];
  then
   echo "removing $hostname from xxx"
   sed -i "/$hostname/d" /opt/hostinfo/infra_hosts
-#  curl --data "host_config_key=16522fa0ee61399df56889b2b3c247f3" http://35.202.229.8:80/api/v2/job_templates/8/callback/
 fi
