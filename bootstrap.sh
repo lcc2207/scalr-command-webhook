b4b1f37336ba15c91e75fe257b79a9df8aacd674
lcc2207 <lynn@fidmail.com>
2019-01-24 10:23:48 -0600

    update tests

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,11 +13,16 @@ then
   apt install python-pip -y
 fi
 
+mkdir -p /var/log/webhook
+appfolder=/opt/command-webhook
+
 pip install docker-compose
 
 curl -fsSL https://get.docker.com/ | sh
 service docker start || systemctl start docker
 
-mkdir -p /var/log/webhook
+git clone https://github.com/scalr-tutorials/scalr-command-webhook.git $appfolder
 
-git clone https://github.com/lcc2207/scalr-command-webhook.git /opt/command-webhook
+chmod 755 $appfolder/relaunch.sh
+cd $appfolder
+./relaunch.sh
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -6,14 +6,14 @@
   end
 end
 
-%w(reporting-webhook_kibana_1 reporting-webhook_elasticsearch_1 reporting-webhook).each do |container|
+%w(command-webhook).each do |container|
   describe docker_container(container) do
     it { should exist }
     it { should be_running }
   end
 end
 
-%w(5018 5601 9200).each do |port|
+%w(5018).each do |port|
   describe port(port) do
     it { should be_listening }
   end
